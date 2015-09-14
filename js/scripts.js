9f28569dffa998bef92ff5d4d94dd337d573d534
Jason Bethel & Phil Shannon <student@epicodus.com>
2015-09-14 15:29:59 -0700

    Add multiple notes functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,29 @@
 $(document).ready(function () {
-  $("#new-notes input").on('change', function() {
-    if ($(this).val().length >= 1) {
-      $("#new-notes").append('<div class="form-group">' +
+  $("#new-notes").on('change', function() {
+    if ($(this).find('input').val().length >= 1) {
+      $("#new-notes").append('<div class="new-note">' +
+                              '<div class="form-group">' +
                               '<input type="text" class="form-control one-note" placeholder="Add note">' +
+                              '</div>' +
                               '</div>');
     }
-    alert("its works");
   });
-  // $("input.notes").change('input', function() {
-    // if ($(this).val().length >= 1) {
-    //   $("#new-notes").append('<div class="form-group">' +
-    //                           '<input type="text" class="form-control notes" placeholder="Add note">' +
-    //                           '</div>');
-    // }
-  //   alert("Hi");
-  // });
-                            //no class nane for new note
+
   $("form#new-place").submit(function(event) {
     event.preventDefault();
 
     var inputtedName = $("input#name").val();
     var inputtedLandmark = $("input#landmarks").val();
     var inputtedSeason = $("input#season").val();
-    var inputtedNote = $("input#notes").val();
-    var newPlace = { placeName: inputtedName, landmark: inputtedLandmark, season: inputtedSeason, note: inputtedNote
+    var newPlace = { placeName: inputtedName, landmark: inputtedLandmark, season: inputtedSeason, note: []
     };
+
+  $(".new-note").each(function() {
+    var inputtedNote = $(this).find("input.one-note").val();
+
+    var newNote = { observation: inputtedNote };
+    newPlace.note.push(newNote);
+  });
 
 
     $("ul#places").append("<li><span class='place'>" + newPlace.placeName + "</span></li>");
@@ -35,12 +34,15 @@ $(document).ready(function () {
       $("#show-place h2").text(newPlace.placeName);
       $(".landmarks").text(newPlace.landmark);
       $(".season").text(newPlace.season);
-      $(".notes").text(newPlace.note);
+      newPlace.note.forEach(function(note) {
+        $("ul#notes").append("<li>" + note.observation + "</li>");
+        debugger;
+      });
     });
 
     $("input#name").val("");
     $("input#landmarks").val("");
     $("input#season").val("");
-    $("input.notes").val("");
+    $("input.one-note").val("");
   });
 });
